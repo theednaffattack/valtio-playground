1c6c5bfeeb264cd8cbb4953bb79806bdd9473d66
Eddie Naff <eddie@eddienaff.dev>
2022-09-23 13:03:40 -0700

    Use a regex for test

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,16 +1,17 @@
-import { render, screen } from "@testing-library/react";
 import { describe, it } from "vitest";
+import { render, screen } from "@testing-library/react";
 import { App } from "./app";
 
 describe("App", () => {
   it("Renders 'Valtio Demo'", () => {
     // Arrange
     render(<App />);
+
     // ACT
 
     // EXPECT
     expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
-      "Valtio Demo"
+      /^Valtio Demo$/
     );
   });
 });
