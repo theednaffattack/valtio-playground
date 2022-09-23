0076a7aee46671a8a5c6f86f67409356516049e0
Eddie Naff <eddie@eddienaff.dev>
2022-09-23 13:04:14 -0700

    Remove extraneous elements

--- src/app.tsx
+++ src/app.tsx
@@ -4,18 +4,8 @@ import { ValtioDemo } from "./valtio-demo";
 export function App() {
   return (
     <div className="App">
-      <header>
-        <nav>
-          <ul>
-            <li>valtio demo</li>
-            <li>docs</li>
-          </ul>
-        </nav>
-      </header>
       <h1>Valtio Demo</h1>
       <ValtioDemo />
-      <h1>Docs</h1>
-      <p>TBD</p>
     </div>
   );
 }
