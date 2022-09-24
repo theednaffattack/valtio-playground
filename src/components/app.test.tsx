0028edcb89ba796651850240b20e249cfc79340f
Eddie Naff <eddie@eddienaff.dev>
2022-09-23 20:43:12 -0700

    Temporary app fix

--- src/components/app.test.tsx
+++ src/components/app.test.tsx
@@ -1,17 +1,32 @@
-import { describe, it } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { App } from "./app";
+import { MemoryRouter } from "react-router-dom";
+import { describe, it } from "vitest";
+import { App, AppWithRouter } from "./app";
 
 describe("App", () => {
   it("Renders 'Valtio Demo'", () => {
     // Arrange
-    render(<App />);
+    render(<AppWithRouter />);
 
     // ACT
 
     // EXPECT
     expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
       /^Valtio Demo$/
+    );
+  });
+
+  it("Renders 'Oops, Page Not Found' if not a valid path", () => {
+    // ARRANGE
+    render(
+      <MemoryRouter initialEntries={["/banana"]}>
+        <App />
+      </MemoryRouter>
+    );
+    // ACT
+    // EXPECT
+    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+      /^Oops, Page Not Found$/
     );
   });
 });
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,24 +1,23 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import { ValtioDemo } from "../valtio-demo";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { FourOhFour } from "../pages/four-oh-four";
 import { Home } from "../pages/home";
+import { ValtioDemo } from "../valtio-demo";
+import "./App.css";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Valtio Demo</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/valtio-demo" element={<ValtioDemo />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<FourOhFour />} />
+      <Route path="/valtio-demo" element={<ValtioDemo />} />
+    </Routes>
   );
 }
 
 export function AppWithRouter() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
